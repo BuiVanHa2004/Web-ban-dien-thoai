fd4c783fa17568754ed1413f1c71a8e5b39478a8
BuiVanHa2004 <20220190@eaut.edu.vn>
2026-06-20 22:52:24 +0700

    feat: add InventoryServiceImpl and OrderStockLogRepository

---
+++ backend/src/main/java/com/webbanhang/shop/Repository/Orders/OrderStockLogRepository.java
@@ -0,0 +1,15 @@
+package com.webbanhang.shop.Repository.Orders;
+
+import com.webbanhang.shop.Model.Orders.OrderStockLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderStockLogRepository extends JpaRepository<OrderStockLog, Integer> {
+    
+    List<OrderStockLog> findByOrderIdOrderByCreatedAtDesc(Integer orderId);
+    
+    List<OrderStockLog> findByVariantIdOrderByCreatedAtDesc(Integer variantId);
+}
